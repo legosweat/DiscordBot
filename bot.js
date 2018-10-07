0e99e230230035d2bfd28a5d775af093227afa59
legosweat <43901277+legosweat@users.noreply.github.com>
2018-10-07 13:05:52 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,13 +2,20 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am ready!');
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
-  	}
+    }
+    if (message.content === 'how to embed') {
+        const embed = new RichEmbed()
+          .setTitle('A slick little embed')
+          .setColor(0xFF0000)
+          .setDescription('Hello, this is a slick embed!');
+        message.channel.send(embed);
+    }
 });
 
 // THIS  MUST  BE  THIS  WAY
